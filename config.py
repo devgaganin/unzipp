1621af3493025b27ea5419274beeae26e507b884
Gagan <business@devgagan.in>
2023-12-23 17:16:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ import os
 
 
 class Config:
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL"))
-    MONGODB_URL = os.environ.get("MONGODB_URL")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    APP_ID = 26075120 #int(os.environ.get("APP_ID"))
+    API_HASH = "1fda88a5d1de46058a4791c78bce198e" #os.environ.get("API_HASH")
+    BOT_TOKEN = "6847425895:AAFp4R_qm2Q-c1HiUsmTPlbP59HtkrnO4iM" #os.environ.get("BOT_TOKEN")
+    LOGS_CHANNEL = "-1002090520438" #int(os.environ.get("LOGS_CHANNEL"))
+    MONGODB_URL = "mongodb+srv://spymusicbot:spymusicbot@cluster0.l4pi5sr.mongodb.net/?retryWrites=true&w=majority" #os.environ.get("MONGODB_URL")
+    BOT_OWNER = "6876018655" #int(os.environ.get("BOT_OWNER"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
     THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
     TG_MAX_SIZE = 2097152000
