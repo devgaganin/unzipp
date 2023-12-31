c090b211bbd22f85a4f068d8fbd0705679d5e1da
Gagan <business@devgagan.in>
2023-12-31 16:28:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ import os
 
 
 class Config:
-    APP_ID = 26075120 #int(os.environ.get("APP_ID"))
-    API_HASH = "1fda88a5d1de46058a4791c78bce198e" #os.environ.get("API_HASH")
-    BOT_TOKEN = "6847425895:AAFp4R_qm2Q-c1HiUsmTPlbP59HtkrnO4iM" #os.environ.get("BOT_TOKEN")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", "-1002090520438"))
-    MONGODB_URL = "mongodb+srv://spymusicbot:spymusicbot@cluster0.l4pi5sr.mongodb.net/?retryWrites=true&w=majority" #os.environ.get("MONGODB_URL")
-    BOT_OWNER = "6876018655" #int(os.environ.get("BOT_OWNER"))
+    APP_ID = gdfge #int(os.environ.get("APP_ID"))
+    API_HASH = "etertert" #os.environ.get("API_HASH")
+    BOT_TOKEN = "gdfggdd:AAFp4R_qm2Q-c1HiUsmTPlbhgrhrgherbfggdP59HtkrnO4iM" #os.environ.get("BOT_TOKEN")
+    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", "-egertert"))
+    MONGODB_URL = "mongofdgfdv://sp12313213324bot:spymusiregtewy546351231`32`cbot@cluster0.l4pi5sr.mongodb.net/?retryWgertwetyrites=true&w=majority" #os.environ.get("MONGODB_URL")
+    BOT_OWNER = "687601w534534655" #int(os.environ.get("BOT_OWNER"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
     THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
     TG_MAX_SIZE = 2097152000
